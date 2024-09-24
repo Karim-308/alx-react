b3cee905f834d617e2a9e9c0f344e2b8b1fdd33a
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-09-25 01:41:03 +0300

    Update 8-seq.js

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,22 +1,24 @@
 import { Seq } from 'immutable';
 
 export default function printBestStudents(object) {
-
   const seq = Seq(object);
 
-
-  const chosen = seq.filter((student) => student.score > 70);
+  const filtered = seq.filter((student) => {
+    student.firstName.charAt(0).toUpperCase();
+    return student.score > 70;
+  });
 
   function capFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
-  const result = chosen.toJS();
-  
-  Object.keys(result).forEach((key) => {
-    result[key].firstName = capFirstLetter(result[key].firstName);
-    result[key].lastName = capFirstLetter(result[key].lastName);
+  const JSObject = filtered.toJS();
+
+  Object.keys(JSObject).map((key) => {
+    JSObject[key].firstName = capFirstLetter(JSObject[key].firstName);
+    JSObject[key].lastName = capFirstLetter(JSObject[key].lastName);
+    return JSObject[key];
   });
 
-  console.log(result);
+  console.log(JSObject);
 }
